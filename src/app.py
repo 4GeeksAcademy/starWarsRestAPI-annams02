2548195adaf6e6d408b7a295f10866487a4c478d
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-05-10 19:01:06 +0000

    terminado los retoques lambda

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User,People,Planets,Favoritos,Personaje_Favoritos,Planeta_favoritos
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,26 +25,138 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_users():
+    users = User.query.all()
+    all_users = list(map(lambda item: item.serialize(),users))   
+    print(all_users)
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:user_id>', methods=['GET'])
+def handle_specific_user(user_id):
+    person = User.query.get(user_id)
+    if person is None:
+        return jsonify({"error": "Person not found"}), 404
+    return jsonify(person.serialize()), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def post_users():
+    request_body_users = request.get_json()   
+    new_body = User(email = request_body_users['email'],password = request_body_users['password'],is_active = request_body_users['is_active'])  
+    db.session.add(new_body)
+    db.session.commit()
+    return jsonify("Usuario creado"), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/people', methods=['GET'])
+def handle_people():
+    people = People.query.all()
+    all_people = list(map(lambda item: item.serialize(), people))
+    print(all_people)
+    return jsonify(all_people), 200
+
+@app.route('/people', methods=['POST'])
+def post_people():
+    request_body_people = request.get_json()   
+    new_body = People(nombre_persona = request_body_people['nombre_persona'],peso = request_body_people['peso'],color_de_piel = request_body_people['color_de_piel'],color_de_pelo = request_body_people['color_de_pelo'],genero = request_body_people['genero'],birth_year = request_body_people['birth_year'])
+    db.session.add(new_body)
+    db.session.commit()
+    # response_body = {
+    #      "msg": "Personaje creado "
+    #  }
+    return jsonify(request_body_people), 200
+    
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_specific_people(people_id):
+    person = People.query.get(people_id)
+    if person is None:
+        return jsonify({"error": "Person not found"}), 404
+    return jsonify({'hola'}), 200
+
+@app.route('/people/<int:people_id>', methods=['DELETE'])
+def delete_people(people_id):
+    person = People.query.get(people_id)
+    if person:
+        db.session.delete(person)
+        db.session.commit()
+        return jsonify({"message": "Persona eliminada exitosamente"}), 200
+    else:
+        return jsonify({"message": "Persona no encontrada"}), 404 
+
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    planets = Planets.query.all()
+    all_planets = list(map(lambda item: item.serialize(), planets))
+    print(all_planets)
+    return jsonify(all_planets), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def handle_specific_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    if planet is None:
+        return jsonify({"error": "Planet not found"}), 404
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planets', methods=['POST'])
+def post_planets():
+    request_body_planets = request.get_json()   
+    new_body = Planets(nombre_planeta = request_body_planets['nombre_planeta'],periodo_rotacion = request_body_planets['periodo_rotacion'],  diametro = request_body_planets['diametro'],clima = request_body_planets['clima'],terreno = request_body_planets['terreno'])
+    db.session.add(new_body)
+    db.session.commit()
+    return jsonify(request_body_planets), 200
+
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify({"message": "Planeta eliminado exitosamente"}), 200
+    else:
+        return jsonify({"message": "Planeta no encontrado"}), 404
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def handle_favorites(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"message": "El usuario no existe"}), 404
+
+    favorites = Favoritos.query.filter_by(usuario_id=user_id).all()
+    serialized_favorites = []
+
+    for favorite in favorites:
+        serialized_favorite = {
+            "id": favorite.id,
+            "planeta": favorite.planeta.nombre_planeta,
+            "personaje": favorite.nombre.nombre_persona
+        }
+        serialized_favorites.append(serialized_favorite)
+
+    return jsonify(serialized_favorites), 200
+
+@app.route('/users/<int:user_id>/personajefavoritos', methods=['POST'])
+def handle_post_personajefavoritos(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"message": "El usuario no existe"}), 404
+    
+    request_body_personje = request.get_json()
+    new_personjefavorito = Personaje_Favoritos(usuario_id = user_id,people_id = request_body_personje["id"])
+
+    db.session.add(new_personjefavorito)
+    db.session.commit()
+
+    return jsonify({"msg":"personaje añadido a favoritos"}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -5,15 +5,107 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    Personaje_Favoritos = db.relationship('Personaje_Favoritos', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active 
+        }
+
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))  
+    usuario = db.relationship('User') 
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planets.id_planeta'))  
+    planeta = db.relationship("Planets")    
+    nombre_id = db.Column(db.Integer, db.ForeignKey('people.id_persona'))  
+    nombre = db.relationship("People")  
+
+class Personaje_Favoritos(db.Model):
+    __tablename__= 'personaje_favoritos'
+    id =db.Column(db.Integer,primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id_persona'))  
+
+    def __repr__(self):
+        return f'<Personaje_Favoritos{self.id}>'
+    
+    def serialize(self):
+        return{
+            "id":self.id,
+            "usuario_id":self.usuario_id,
+            "people_id":self.people_id,
+        }  
+    
+class Planeta_favoritos(db.Model):
+    __tablename__= 'planeta_favoritos'
+    id =db.Column(db.Integer,primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planets.id_planeta'))  
+
+    def __repr__(self):
+        return f'<Planeta_favoritos{self.id}>'
+    
+    def serialize(self):
+        return{
+            "id":self.id,
+            "usuario_id":self.usuario_id,
+            "planet_id":self.planet_id
+        }  
+    
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id_persona = db.Column(db.Integer, primary_key=True)
+    nombre_persona = db.Column(db.String(100), nullable=False)
+    peso = db.Column(db.Integer, nullable=False)
+    color_de_piel = db.Column(db.String(20), nullable=False)
+    color_de_pelo = db.Column(db.String(20))
+    genero = db.Column(db.String(20))
+    birth_year = db.Column(db.Integer, nullable=False)
+    Personaje_Favoritos = db.relationship('Personaje_Favoritos', backref='People', lazy=True)
+
+    def __repr__(self):
+        return f'<People {self.nombre_persona}>'
+
+    def serialize(self):
+        return {
+            "id_persona": self.id_persona,
+            "nombre_persona": self.nombre_persona,
+            "peso": self.peso,
+            "color_de_piel": self.color_de_piel,
+            "color_de_pelo": self.color_de_pelo,
+            "genero": self.genero,
+            "birth_year": self.birth_year
+          
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id_planeta = db.Column(db.Integer, primary_key=True)
+    nombre_planeta = db.Column(db.String(100), nullable=False)
+    periodo_rotacion = db.Column(db.Integer, nullable=False)
+    diametro = db.Column(db.Float, nullable=False)
+    clima = db.Column(db.String(50), nullable=False)
+    terreno = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Planets {self.nombre_planeta}>'
+
+    def serialize(self):
+        return {
+            "id_planeta": self.id_planeta,
+            "nombre_planeta": self.nombre_planeta,
+            "periodo_rotacion": self.periodo_rotacion,
+            "diametro": self.diametro,
+            "clima": self.clima,
+            "terreno": self.terreno
         }
